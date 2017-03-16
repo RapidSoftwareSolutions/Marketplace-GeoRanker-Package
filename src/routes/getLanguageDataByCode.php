0e5676d4f3f90a5de3511579cfbc307e6a2f04cb
ustal <quit.ipad2@gmail.com>
2017-03-16 14:17:47 +0200

    fix metadata

--- src/routes/getLanguageDataByCode.php
+++ src/routes/getLanguageDataByCode.php
@@ -1,21 +1,21 @@
 <?php
 
-$app->post('/api/GeoRanker/getLanguageDataByCode', function ($request, $response, $args) {
+$app->post('/api/GeoRanker/getLanguageDataByCode', function ($request, $response) {
     /** @var \Slim\Http\Response $response */
     /** @var \Slim\Http\Request $request */
     /** @var \Models\checkRequest $checkRequest */
 
     $settings = $this->settings;
     $checkRequest = $this->validation;
-    $validateRes = $checkRequest->validate($request, ['email', 'session', 'code']);
+    $validateRes = $checkRequest->validate($request, ['email', 'session', 'countryCode']);
     if (!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback'] == 'error') {
         return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
     } else {
         $postData = $validateRes;
     }
 
-    $url = $settings['apiUrl'] . "/language/" . $postData['args']['code'] . ".json?";
-    unset($postData['args']['code']);
+    $url = $settings['apiUrl'] . "/language/" . $postData['args']['countryCode'] . ".json?";
+    unset($postData['args']['countryCode']);
 
     try {
         /** @var GuzzleHttp\Client $client */
